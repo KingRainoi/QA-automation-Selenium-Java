59ee85967c4db75a9384f1b27610c2dab8a6003d
Rubén Gamboa <bezyex@gmail.com>
2024-01-02 11:44:33 -0600

    Shopping cart

--- Java+Selenium/basic/src/main/java/learning/shoppingCart.java
+++ Java+Selenium/basic/src/main/java/learning/shoppingCart.java
@@ -1,6 +1,10 @@
 package learning;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class shoppingCart {
@@ -8,8 +12,21 @@ public class shoppingCart {
         System.setProperty("webdriver.chrome.driver","C:\\Development\\Drivers\\geckodriver-v0.33.0-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-        
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 
+        //List of things to buy
+        String[] itemsNeeded = {"Cucumber","Brocolli","Beetroot"};
+
+        //List of all the products available
+        List<WebElement> products =  driver.findElements(By.cssSelector("h4.product-name"));
+        for(int i=0;i<products.size();i++)
+        {
+            String product = products.get(i).getText();
+            for(String item : itemsNeeded) {
+                if(product.contains(item)) {
+                    driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+                }
+            }
+        }
     }
 }
